e7fc21d214567f1a86bd3aa7004c541fd3be8aa7
eepyemy <emy.ict@proton.me>
2025-10-23 20:47:30 +0200

    added new things

--- .test.c
+++ .test.c
@@ -6,7 +6,7 @@
 /*   By: emkulpa <emkulpa@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 15:29:39 by emkulpa           #+#    #+#             */
-/*   Updated: 2025/10/15 18:04:36 by emkulpa          ###   ########.fr       */
+/*   Updated: 2025/10/23 20:46:42 by emkulpa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,17 @@
 #define FOR_EACH_8(up, down, x, ...) up(down(x));FOR_EACH_7(up, down, __VA_ARGS__)
 #define FOR_EACH_9(up, down, x, ...) up(down(x));FOR_EACH_8(up, down, __VA_ARGS__)
 
+#define FOR_EACH_MULTI_0(up, down)
+#define FOR_EACH_MULTI_1(up, down, x) up(down##x); 
+#define FOR_EACH_MULTI_2(up, down, x, ...) up(down##x);FOR_EACH_MULTI_1(up, down, __VA_ARGS__)
+#define FOR_EACH_MULTI_3(up, down, x, ...) up(down##x);FOR_EACH_MULTI_2(up, down, __VA_ARGS__)
+#define FOR_EACH_MULTI_4(up, down, x, ...) up(down##x);FOR_EACH_MULTI_3(up, down, __VA_ARGS__)
+#define FOR_EACH_MULTI_5(up, down, x, ...) up(down##x);FOR_EACH_MULTI_4(up, down, __VA_ARGS__)
+#define FOR_EACH_MULTI_6(up, down, x, ...) up(down##x);FOR_EACH_MULTI_5(up, down, __VA_ARGS__)
+#define FOR_EACH_MULTI_7(up, down, x, ...) up(down##x);FOR_EACH_MULTI_6(up, down, __VA_ARGS__)
+#define FOR_EACH_MULTI_8(up, down, x, ...) up(down##x);FOR_EACH_MULTI_7(up, down, __VA_ARGS__)
+#define FOR_EACH_MULTI_9(up, down, x, ...) up(down##x);FOR_EACH_MULTI_8(up, down, __VA_ARGS__)
+
 // test macros
 #define TO_STR(x) #x
 #define CMP_FLAT(x, y) ((x != 0) && (y != 0)) || ((x == 0) && (y == 0)) ? 1 : 0
@@ -59,6 +70,8 @@
 #define header_message(x) printf(PINK "\n\n[%s]" DEFAULT, x)
 #define memprint(start, n) announce(_memprint(start, n), " ")
 #define _memprint(start, n) memprint_indent(start, " ", n)
+#define memprint_char(start, n) announce(_memprint_char(start, n), " ")
+#define _memprint_char(start, n) memprint_char_indent(start, " ", n)
 #define test_is(func, n, ...) \
 header_message(TO_STR(ft_##func)); \
 FOR_EACH_##n(assert_ctypeismore, func, __VA_ARGS__);
@@ -66,6 +79,10 @@ FOR_EACH_##n(assert_ctypeismore, func, __VA_ARGS__);
 #define test(func, n, ...) \
 header_message(TO_STR(ft_##func)); \
 FOR_EACH_##n(assert_ctype, func, __VA_ARGS__);
+
+#define test_multi(func, n, ...) \
+header_message(TO_STR(ft_##func)); \
+FOR_EACH_MULTI_##n(assert_ctype, func, __VA_ARGS__);
 
 int	cmp_flat(int arg1, int arg2) { return CMP_FLAT(arg1, arg2); }
 int cmp(int x, int y) { return x == y; }
@@ -99,9 +116,23 @@ void	memprint_indent(void *start, const char* indent, t_size n)
 	i = 0;
 	while (i < n)
 	{
-		if (i % 8 == 0)
+		if (i % 16 == 0)
 			printf("\n" "%s", indent);
 		printf(" %02x", *((char *)(start+i)));
+		i++;
+	}
+}
+
+void	memprint_char_indent(void *start, const char* indent, t_size n)
+{
+	t_size	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i % 16 == 0)
+			printf("\n" "%s", indent);
+		printf(" %c", *((char *)(start+i)));
 		i++;
 	}
 }
@@ -128,7 +159,7 @@ void	test_ft_isascii(void)
 
 void	test_ft_isprint(void)
 {
-	test_is(isprint, 9, -1, 128, 'A', 'Z', 'z', '\n', '\t', '9', 31);
+	test_is(isprint, 9, -1, 127, ' ', 'Z', 'z', '\n', '\t', '9', 31);
 }
 
 void	test_ft_strlen(void)
@@ -144,6 +175,12 @@ void	test_ft_strlen(void)
 	printf("\n"); memprint(m1, n); memprint(m2, n); 
 #define mem_test(func, m1, m2, n, ...) printf("\n"); announce(func(m1, __VA_ARGS__), " "); \
 	announce(ft_##func(m2, __VA_ARGS__), " "); assert(memcmp(m1, m2, n) == 0);
+#define strlcpy_test_verbose(func, m1, m2, n, ...) printf("\n--------------------------------------------"); \
+	memprint(m1, n); memprint(m2, n); \
+	strlcpy_test(func, m1, m2, n, __VA_ARGS__); \
+	printf("\n"); memprint(m1, n); memprint(m2, n); 
+#define strlcpy_test(func, m1, m2, n, ...) printf("\n"); \
+	assert(ft_##func(m2, __VA_ARGS__) == func(m1, __VA_ARGS__)); assert(memcmp(m1, m2, n) == 0);
 
 void	test_ft_memset(void)
 {
@@ -190,7 +227,7 @@ void	test_ft_bzero(void)
 void	test_ft_memcpy(void)
 {
 	char tempo[12] = "hello world";
-	char tempo2[12] = "hello world";
+	// char tempo2[12] = "hello world";
 	char m1[5] = "1234";
 	char m2[5] = "1234";
 	char m3[9] = "12345678";
@@ -201,89 +238,125 @@ void	test_ft_memcpy(void)
 	char m8[1] = "";
 	char m9[2] = "1";
 	char m10[2] = "1";
-	char *m11;
-	char *m12;
-	m11 = tempo + 1;
-	m12 = tempo2 + 1;
+	// char *m11;
+	// char *m12;
+	// m11 = tempo + 1;
+	// m12 = tempo2 + 1;
+	// char *m13 = (void *)0;
+	// char *m14 = (void *)0;
+	// char *m15 = (void *)0;
 	header_message("ft_memcpy");
 	mem_test_verbose(memcpy, m1, m2, 5, tempo, 3);
 	mem_test_verbose(memcpy, m3, m4, 9, tempo, 5);
 	mem_test(memcpy, m5, m6, 4, tempo, 2);
 	mem_test(memcpy, m7, m8, 1, tempo, 1);
 	mem_test(memcpy, m9, m10, 2, tempo, 2);
-	
-	// overlap
-	memprint(m11, 4);
-	memprint(m12, 4);
-	memprint(tempo, 11);
-	memprint(tempo2, 11);
-	announce(memcpy(m11, tempo, 4), " ");
-	announce(ft_memcpy(m12, tempo2, 4), " ");
-	memprint(m11, 4);
-	memprint(m12, 4);
-	memprint(tempo, 11);
-	memprint(tempo2, 11);
-	assert(memcmp(m11, m12, 4) == 0);
+	// mem_test(memcpy, m13, m14, 0, m15, 0);
+	// // overlap
+	// memprint(m11, 4);
+	// memprint(m12, 4);
+	// memprint(tempo, 11);
+	// memprint(tempo2, 11);
+	// announce(memcpy(m11, tempo, 4), " ");
+	// announce(ft_memcpy(m12, tempo2, 4), " ");
+	// memprint(m11, 4);
+	// memprint(m12, 4);
+	// memprint(tempo, 11);
+	// memprint(tempo2, 11);
+	// assert(memcmp(m11, m12, 4) == 0);
 }
 
 void	test_ft_memmove(void)
 {
-	char tempo[12] = "hello world";
-	char tempo2[12] = "hello world";
-	char m1[5] = "1234";
-	char m2[5] = "1234";
-	char m3[9] = "12345678";
-	char m4[9] = "12345678";
-	char m5[4] = "\0\0\0";
-	char m6[4] = "\0\0\0";
-	char m7[1] = "";
-	char m8[1] = "";
-	char m9[2] = "1";
-	char m10[2] = "1";
-	char *m11;
-	char *m12;
-	m11 = tempo + 1;
-	m12 = tempo2 + 1;
+	char src1[12] = "hello world";
+	char src2[12] = "hello world";
+	char dest1[5] = "1234";
+	char dest2[5] = "1234";
+	char dest3[9] = "12345678";
+	char dest4[9] = "12345678";
+	char dest5[4] = "\0\0\0";
+	char dest6[4] = "\0\0\0";
+	char dest7[1] = "";
+	char dest8[1] = "";
+	char dest9[2] = "1";
+	char dest10[2] = "1";
+	char *dest11;
+	char *dest12;
+	char *src3;
+	char *src4;
+
+	dest11 = src1 + 1;
+	dest12 = src2 + 1;
+	src3 = src1 + 5;
+	src4 = src2 + 5;
 	header_message("ft_memmove");
-	mem_test_verbose(memmove, m1, m2, 5, tempo, 3);
-	mem_test_verbose(memmove, m3, m4, 9, tempo, 5);
-	mem_test(memmove, m5, m6, 4, tempo, 2);
-	mem_test(memmove, m7, m8, 1, tempo, 1);
-	mem_test(memmove, m9, m10, 2, tempo, 2);
+	mem_test_verbose(memmove, dest1, dest2, 5, src1, 3);
+	mem_test_verbose(memmove, dest3, dest4, 9, src1, 5);
+	mem_test(memmove, dest5, dest6, 4, src1, 2);
+	mem_test(memmove, dest7, dest8, 1, src1, 1);
+	mem_test(memmove, dest9, dest10, 2, src1, 2);
 
 	// overlap
-	memprint(m11, 4);
-	memprint(m12, 4);
-	memprint(tempo, 11);
-	memprint(tempo2, 11);
-	announce(memmove(m11, tempo, 4), " ");
-	announce(ft_memmove(m12, tempo2, 4), " ");
-	memprint(m11, 4);
-	memprint(m12, 4);
-	memprint(tempo, 11);
-	memprint(tempo2, 11);
-	assert(memcmp(m11, m12, 4) == 0);
+	memprint(src1, 11);
+	memprint(dest11, 4);
+	memprint(dest12, 4);
+	memprint(src3, 11);
+	memprint(src4, 11);
+	announce(memmove(dest11, src3, 6), " ");
+	announce(ft_memmove(dest12, src4, 6), " ");
+	memprint(dest11, 11);
+	memprint(dest12, 11);
+	memprint(src1, 11);
+	memprint(src3, 11);
+	memprint(src2, 11);
+	assert(memcmp(dest11, dest12, 6) == 0);
 }
 
 void	test_ft_strlcpy(void)
 {
-	char tempo[12] = "hello world";
+	char s1[12] = "hello world";
 	//char tempo2[12] = "hello world";
-	char m1[5] = "\0abc";
-	char m2[5] = "\0abc";
-	char m3[9] = "12345678";
-	char m4[9] = "12345678";
-	char m5[16] = "123456789abcde";
-	char m6[16] = "123456789abcde";
+	char d1[5] = "\0abc";
+	char d2[5] = "\0abc";
+	char d3[9] = "12345678";
+	char d4[9] = "12345678";
+	char d5[16] = "123456789abcde";
+	char d6[16] = "123456789abcde";
 	// char m7[1] = "";
 	// char m8[1] = "";
 	// char m9[2] = "1";
 	// char m10[2] = "1";
 	header_message("ft_strlcpy");
-	memprint(tempo, 12);
-	mem_test_verbose(strlcpy, m1, m2, 8, tempo, 8)
-	mem_test_verbose(strlcpy, m3, m4, 16, tempo, 9)
-	mem_test_verbose(strlcpy, m5, m6, 16, tempo, 16)
+	memprint(s1, 12);
+	printf("\n--------------------------------------------");
+	memprint(d1, 5);
+	memprint(d2, 5);
+	printf("\n"); 
+	assert(ft_strlcpy(d2, s1, 4) == strlcpy(d1, s1, 4)); 
+	assert(memcmp(d1, d2, 5) == 0);
+	printf("\n"); 
+	memprint(d1, 5); 
+	memprint(d2, 5);
+
+	printf("\n--------------------------------------------");
+	memprint(d3, 9);
+	memprint(d4, 9);
+	printf("\n"); 
+	assert(ft_strlcpy(d4, s1, 4) == strlcpy(d3, s1, 4)); 
+	assert(memcmp(d3, d4, 9) == 0);
+	printf("\n"); 
+	memprint(d3, 9); 
+	memprint(d4, 9);
+
+	printf("\n--------------------------------------------");
+	memprint(d5, 5);
+	memprint(d6, 5);
+	printf("\n"); 
+	assert(ft_strlcpy(d6, s1, 0) == strlcpy(d5, s1, 0)); 
+	assert(memcmp(d5, d6, 5) == 0);
+	printf("\n"); 
+	memprint(d5, 5); 
+	memprint(d6, 5);
 }
 
 void	test_ft_strlcat(void)
@@ -293,12 +366,12 @@ void	test_ft_strlcat(void)
 
 void	test_ft_toupper(void)
 {
-
+	test(toupper, 6, 'a', 'b', 'z', 'A', 'Z', ' ')
 }
 
 void	test_ft_tolower(void)
 {
-
+	test(tolower, 6, 'a', 'b', 'z', 'A', 'Z', ' ')
 }
 
 void	test_ft_strchr(void)
@@ -313,7 +386,7 @@ void	test_ft_strrchr(void)
 
 void	test_ft_strncmp(void)
 {
-
+	test_multi(strncmp, 2, ("abc", "abcd", 3), ("", "abc", 4));	
 }
 
 void	test_ft_memchr(void)
